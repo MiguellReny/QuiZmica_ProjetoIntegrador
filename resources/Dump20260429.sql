ac9eb039facc7c00aa4dc0c48a7be4b2a3742943
BrenoRossini <brenorossinimaua@gmail.com>
2026-06-02 17:51:17 -0300

    feat: conexao banco concluida

--- resources/Dump20260429.sql
+++ resources/Dump20260429.sql
@@ -3,7 +3,6 @@
 -- Host: quizmica-brenorossinimaua-7324.d.aivencloud.com    Database: defaultdb
 -- ------------------------------------------------------
 -- Server version	8.0.45
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
