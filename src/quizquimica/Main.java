fb00dee37c5c3640aef5819333aae3ce871ef865
BrenoRossini <brenorossinimaua@gmail.com>
2026-06-02 17:05:21 -0300

    fix: renomeando arquivos

--- src/quizquimica/Main.java
+++ src/quizquimica/Main.java
@@ -1,12 +1,12 @@
 package quizquimica;
 
-import quizquimica.controller.AuthController;
+import quizquimica.controller.AuthControllerBack;
 import quizquimica.model.Usuario;
 
 public class Main {
     public static void main(String[] args) {
 
-        AuthController auth = new AuthController();
+        AuthControllerBack auth = new AuthControllerBack();
 
         Usuario prof = auth.realizarLogin("mariadosocorro@cps.sp.gov.br", "482951");
         System.out.println(prof != null ? "Conectado ao Aiven! Login OK: " + prof.getNome() : "FALHOU");
